d2402e2600cebd8a2d463e9b77d38a2a2e914229
electron271 <aveanzaldua@gmail.com>
2023-02-18 20:18:46 -0600

    fix warnings, add comments

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
+// src/main.rs
+// Main file
+// Imports
 use slog::{o, Drain};
 use smithay::reexports::{calloop::EventLoop, wayland_server::Display};
 use state::Corrosion;
 mod handlers;
 mod state;
 
+// Define the CalloopData struct
 pub struct CalloopData {
     state: Corrosion,
     display: Display<Corrosion>,
 }
+
 // The &String in Result<> is a placeholder for now
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let logger = slog::Logger::root(slog_stdlog::StdLog.fuse(), o!());
-    let mut event_loop: EventLoop<CalloopData> = EventLoop::try_new()?;
+    let _logger = slog::Logger::root(slog_stdlog::StdLog.fuse(), o!());
+    let _event_loop: EventLoop<CalloopData> = EventLoop::try_new()?;
 
-    let display = Display::<Corrosion>::new().unwrap();
+    let _display = Display::<Corrosion>::new().unwrap();
     Ok(())
 }
