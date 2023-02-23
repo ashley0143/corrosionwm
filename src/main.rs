e393b72511b639fffba70c8a79eb050a4ad81b15
a weeb programmer <65640526+a-weeb-programmer@users.noreply.github.com>
2023-02-23 16:22:52 -0700

    added tracing subscriber

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,12 @@ pub struct CalloopData {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-
+    if let Ok(env_filter) = tracing_subscriber::EnvFilter::try_from_default_env() {
+        tracing_subscriber::fmt().with_env_filter(env_filter).init();
+    } else {
+        tracing_subscriber::fmt().init();
+    }
+    
     let mut event_loop: EventLoop<CalloopData> = EventLoop::try_new()?;
 
     let mut display: Display<Corrosion> = Display::new()?;
